5543a409a6b6e56580080a991090f57c8ddc89f9
susuko <flokinyan@gmail.com>
2024-03-21 11:06:42 +0900

    update control

--- control.c
+++ control.c
@@ -8,11 +8,12 @@ void startControlRobot(void)
 	const uint32_t time_limit = 500;
 	
 	int last_dir = 0;
-	uint32_t last_dir_time = millis() - time_limit;
-	uint32_t last_straight_time = millis() - time_limit;
+	uint32_t start_time = millis();
+	int64_t last_dir_time = INT32_MIN;
+	int64_t last_straight_time = INT32_MIN;
 	
 	while (1) {
-		uint32_t now = millis();
+		uint32_t now = millis() - start_time;
 		double line_dist = getLineDist();
 		
 		if (getNearDistanceWarning()) {
@@ -38,10 +39,17 @@ void startControlRobot(void)
 			if (now < last_dir_time + time_limit) {
 				setMotorXy(last_dir * 0.75, 1.0);
 			}
+			else if (now < last_dir_time + time_limit * 2) {
+				setMotorXy(last_dir * 0.75, -1.0);
+			}
+			else if (now < last_dir_time + time_limit * 3) {
+				setMotorXy(last_dir * -0.75, 1.0);
+			}
 			else {
 				setMotorXy(0.0, 0.0);
 			}
 		}
+		
 		delay(10);
 	}
 }
