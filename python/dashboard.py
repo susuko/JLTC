714a6145fc6c5a84fd1c28a12150f7074365c0be
susuko <flokinyan@gmail.com>
2024-03-24 02:24:40 +0900

    update dashboard

--- python/dashboard.py
+++ python/dashboard.py
@@ -1,119 +1,152 @@
 import tkinter as tk
 import fileinput
-import threading
+from threading import Thread
 from time import sleep
 from collections import namedtuple
+from collections import defaultdict
 
+# NamedTuple definition
 Size = namedtuple("Size", ["width", "height"])
 Point = namedtuple("Point", ["x", "y"])
 Rect = namedtuple("Rect", ["offset", "size"])
+Font = namedtuple("Font", ["family", "size"])
 
-def create_round_rectangle(x0, y0, x1, y1, r, **kwargs):
-    points = [
-        x0 + r, y0, x1 - r, y0, x1, y0, x1, y0 + r, x1, y1 - r, x1, y1,
-        x1 - r, y1, x0 + r, y1, x0, y1, x0, y1 - r, x0, y0 + r, x0, y0
-    ]
-    return canvas.create_polygon(points, **kwargs, smooth = True)
+class Dashboard(tk.Tk):
+    # Canvas constant
+    CANVAS_SIZE = Size(width=200, height=300)
+    CANVAS_SCALE = 2.2
 
-def rect_to_points(rect):
-    canvas_mid = Point(x = canvas_size.width / 2, y = canvas_size.height / 2)
-    rect_mid = Point(x = rect.size.width / 2, y = rect.size.height / 2)
-    return (
-        canvas_mid.x - rect_mid.x + rect.offset.x,
-        canvas_mid.y - rect_mid.y + rect.offset.y,
-        canvas_mid.x + rect_mid.x + rect.offset.x,
-        canvas_mid.y + rect_mid.y + rect.offset.y
-    )
+    # Font constant
+    FONT = Font("Consolas", 18)
 
-def input_thread():
-    for line in fileinput.input():
-        time_str, command, *args = [s.strip() for s in line.split(",")]
-        time = int(time_str)
-        if (command == "setMotor"):
-            set_motor(args)
-        elif (command == "nearDistanceWarningThread"):
-            near_distance_warning_thread(args)
-        sleep(0.01)
+    # Color constant
+    BG_COLOR = "#0f160d"
+    FONT_COLOR = "#e2fcde"
+    SHAPE_COLOR = "#162113"
 
-def set_motor(args):
-    left, right = [int(float(arg) * 100) for arg in args]
-    left_text = tire_text_format % left
-    right_text = tire_text_format % right
-    canvas.itemconfig(left_tire_text, text = left_text)
-    canvas.itemconfig(right_tire_text, text = right_text)
+    # Text format constant
+    TIRE_TEXT_FORMAT = "%3d\n[%%]"
+    DIST_TEXT_FORMAT = "> DISTANCE: %.2f[CM]"
 
-def near_distance_warning_thread(args):
-    dist = float(args[0])
-    dist_label.config(text = dist_text_format % dist)
+    # Rect constant
+    BOARD_RECT = Rect(Point(0, 0), Size(100, 200))
+    LEFT_TIRE_RECT = Rect(Point(-70, 70), Size(30, 60))
+    RIGHT_TIRE_RECT = Rect(Point(70, 70), Size(30, 60))
 
-bg_color = "#212121"
-font_name = "Consolas"
-font = (font_name, 18)
-font_color = "#ffffff"
-tire_text_format = "%3d\n[%%]"
-dist_text_format = "DISTANCE: %.2f[CM]"
+    def __init__(self):
+        super().__init__()
 
-root = tk.Tk()
-root.title("JLTC")
-root.geometry("1280x720")
-root.config(bg = bg_color)
+        # Initialize tkinter
+        self.title("JLTC")
+        self.geometry("1280x720")
+        self.config(bg=self.BG_COLOR)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
-canvas_size = Size(width = 200, height = 300)
-canvas_scale = 2.4
-canvas = tk.Canvas(
-    width = canvas_size.width * canvas_scale,
-    height = canvas_size.height * canvas_scale,
-    highlightthickness = 0
-)
-canvas.config(bg = bg_color)
+        # Initialize frames
+        canvas_frame = tk.LabelFrame(
+            self, text="VIEW", bg=self.BG_COLOR, fg=self.FONT_COLOR, font=self.FONT
+        )
+        canvas_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.create_canvas(canvas_frame)
 
-frame_rect = Rect(Point(0, 0), Size(190, 290))
-main_board_rect = Rect(Point(0, 0), Size(100, 200))
-left_tire_rect = Rect(Point(-65, 70), Size(30, 60))
-right_tire_rect = Rect(Point(65, 70), Size(30, 60))
-lcd_rect = Rect(Point(0, -50), Size(100, 40))
+        info_frame = tk.LabelFrame(
+            self, text="INFO", bg=self.BG_COLOR, fg=self.FONT_COLOR, font=self.FONT
+        )
+        info_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+        info_inner_frame = tk.Frame(info_frame, bg=self.BG_COLOR)
+        info_inner_frame.pack(side="left", padx=10, pady=10, fill="y")
+        self.create_distance_label(info_inner_frame)
 
-left_tire_points = rect_to_points(left_tire_rect)
-left_tire_mid_point = (
-    (left_tire_points[0] + left_tire_points[2]) / 2,
-    (left_tire_points[1] + left_tire_points[3]) / 2
-)
-right_tire_points = rect_to_points(right_tire_rect)
-right_tire_mid_point = (
-    (right_tire_points[0] + right_tire_points[2]) / 2,
-    (right_tire_points[1] + right_tire_points[3]) / 2
-)
+        # Initialize input thread
+        Thread(target=self.input_thread).start()
 
-frame = create_round_rectangle(
-    *rect_to_points(frame_rect), r = 5, fill = "#2f2f2f"
-)
-main_board = create_round_rectangle(
-    *rect_to_points(main_board_rect), r = 5, fill = "#b8b8b8"
-)
-lcd = canvas.create_rectangle(
-    rect_to_points(lcd_rect), fill = "#a2c3c9"
-)
-left_tire = create_round_rectangle(
-    *left_tire_points, r = 5, fill = "#000000"
-)
-left_tire_text = canvas.create_text(
-    left_tire_mid_point, text = tire_text_format % 0, font = font, fill = font_color
-)
-right_tire = create_round_rectangle(
-    *right_tire_points, r = 5, fill = "#000000"
-)
-right_tire_text = canvas.create_text(
-    right_tire_mid_point, text = tire_text_format % 0, font = font, fill = font_color
-)
+    def create_canvas(self, parent):
+        self.canvas = tk.Canvas(
+            parent,
+            width=self.CANVAS_SIZE.width * self.CANVAS_SCALE,
+            height=self.CANVAS_SIZE.height * self.CANVAS_SCALE,
+            highlightthickness=0,
+            bg=self.BG_COLOR
+        )
+        self.create_canvas_elements()
+        self.canvas.scale(tk.ALL, 0, 0, self.CANVAS_SCALE, self.CANVAS_SCALE)
+        self.canvas.pack()
 
-canvas.scale(tk.ALL, 0, 0, canvas_scale, canvas_scale)
-canvas.grid(row = 0, column = 0)
+    def create_canvas_elements(self):
+        self.board = self.create_board(self.BOARD_RECT)
+        self.left_tire, self.left_tire_text = self.create_tire(self.LEFT_TIRE_RECT)
+        self.right_tire, self.right_tire_text = self.create_tire(self.RIGHT_TIRE_RECT)
 
-dist_label = tk.Label(
-    text = dist_text_format % 0, bg = bg_color, fg = font_color, font = font
-)
-dist_label.grid(row = 0, column = 1, pady = 10, sticky = tk.N)
+    def create_board(self, rect):
+        return self._canvas_create_round_rectangle(rect, r=5, fill=self.SHAPE_COLOR)
 
-threading.Thread(target = input_thread).start()
+    def create_tire(self, rect):
+        (x0, y0), (x1, y1) = self._rect_to_points(rect)
+        tire = self._canvas_create_round_rectangle(rect, r=5, fill=self.SHAPE_COLOR)
+        tire_mid_point = ((x0 + x1) / 2, (y0 + y1) / 2)
+        tire_text = self.canvas.create_text(
+            tire_mid_point,
+            text=self.TIRE_TEXT_FORMAT % 0,
+            font=self.FONT,
+            fill=self.FONT_COLOR
+        )
+        return (tire, tire_text)
 
-root.mainloop()
+    def _canvas_create_round_rectangle(self, rect, r, **kwargs):
+        (x0, y0), (x1, y1) = self._rect_to_points(rect)
+        return self.canvas.create_polygon(
+            x0 + r, y0, x1 - r, y0, x1, y0, x1, y0 + r, x1, y1 - r, x1, y1,
+            x1 - r, y1, x0 + r, y1, x0, y1, x0, y1 - r, x0, y0 + r, x0, y0,
+            **kwargs, smooth=True
+        )
+
+    def _rect_to_points(self, rect):
+        canvas_mid = Point(x=self.CANVAS_SIZE.width / 2, y=self.CANVAS_SIZE.height / 2)
+        rect_mid = Point(x=rect.size.width / 2, y=rect.size.height / 2)
+        return [
+            Point(
+                canvas_mid.x - rect_mid.x + rect.offset.x,
+                canvas_mid.y - rect_mid.y + rect.offset.y
+            ),
+            Point(
+                canvas_mid.x + rect_mid.x + rect.offset.x,
+                canvas_mid.y + rect_mid.y + rect.offset.y
+            )
+        ]
+
+    def create_distance_label(self, parent):
+        self.dist_label = tk.Label(
+            parent,
+            text=self.DIST_TEXT_FORMAT % 0,
+            bg=self.BG_COLOR,
+            fg=self.FONT_COLOR,
+            font=self.FONT
+        )
+        self.dist_label.pack()
+
+    # Input monitoring from standard input
+    def input_thread(self):
+        command_handlers = {
+            "setMotor": self.update_set_motor,
+            "nearDistanceWarningThread": self.update_near_distance_warning_thread
+        }
+        for line in fileinput.input():
+            _, command, *args = [s.strip() for s in line.split(",")]
+            command_handlers[command](args)
+            sleep(0.001)
+
+    def update_set_motor(self, args):
+        left, right = [int(float(arg) * 100) for arg in args]
+        left_text = self.TIRE_TEXT_FORMAT % left
+        right_text = self.TIRE_TEXT_FORMAT % right
+        self.canvas.itemconfig(self.left_tire_text, text=left_text)
+        self.canvas.itemconfig(self.right_tire_text, text=right_text)
+
+    def update_near_distance_warning_thread(self, args):
+        dist = float(args[0])
+        self.dist_label.config(text=self.DIST_TEXT_FORMAT % dist)
+
+if __name__ == "__main__":
+    app = Dashboard()
+    app.mainloop()
