59e0ce3e183ddb104fb19e1b761242419cfe08f5
susuko <flokinyan@gmail.com>
2024-03-26 10:14:33 +0900

    update shutdown

--- shutdown.c
+++ shutdown.c
@@ -8,6 +8,11 @@ static int _lcd_fd;
 
 static noreturn void shutdown(void)
 {
+	logPrintf("shutdown", "1");
+	playEndBeep();
+	clearLcd(_lcd_fd);
+	putsLcd(_lcd_fd, "SHUTDOWN...");
+	
 	system("shutdown -h now");
 	exit(0);
 }
@@ -19,11 +24,6 @@ static PI_THREAD (shutdownManagementThread)
 		counter = getRedButton() ? counter + 1 : 0;
 		delay(1000);
 	}
-	
-	playEndBeep();
-	
-	clearLcd(_lcd_fd);
-	putsLcd(_lcd_fd, "SHUTDOWN...");
 	
 	shutdown();
 	
