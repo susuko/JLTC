62f3376b52ea978932092fecb62eeb4564a212b9
susuko <flokinyan@gmail.com>
2024-03-28 12:23:04 +0900

    update distance

--- distance.c
+++ distance.c
@@ -5,23 +5,42 @@
 
 // Warning threshold [cm]
 static const double DIST_TH = 15;
+static const int WARNING_COUNT = 4;
 
 static int _dist_fd;
 static int _distance_warning = 0;
+
+static void updateDistanceWarning(double prev_dist, double now_dist)
+{
+	static int warning_count = 0;
+	
+	if (prev_dist < DIST_TH && now_dist < DIST_TH) {
+		warning_count = WARNING_COUNT;
+	}
+	
+	if (warning_count > 0) {
+		_distance_warning = 1;
+		warning_count--;
+	}
+	else {
+		_distance_warning = 0;
+	}
+}
 
 static PI_THREAD (distanceMonitoringThread)
 {
 	double prev_dist = 0;
 	
 	while (1) {
-		double dist = readDist(_dist_fd);
-		_distance_warning = prev_dist < DIST_TH && dist < DIST_TH;
+		double now_dist = readDist(_dist_fd);
 		
-		if (prev_dist != dist) {
-			logPrintf("distanceMonitoringThread", "%.2f", dist);
+		updateDistanceWarning(prev_dist, now_dist);
+		
+		if (prev_dist != now_dist) {
+			logPrintf("distanceMonitoringThread", "%.2f", now_dist);
 		}
 		
-		prev_dist = dist;
+		prev_dist = now_dist;
 		delay(50);
 	}
 	
